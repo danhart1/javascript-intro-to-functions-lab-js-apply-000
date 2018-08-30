21e275b1606cc0587467f8517c34fe9288feaf77
danhart1 <danhart249@live.com>
2018-08-30 17:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string){
     return("I can't hear you!");
     }else if(string.toUpperCase()===string){
       return("YES INDEED!");
-    }else if((string ==='I Love you, Grandma.')){
+    }else if(string =='I Love you, Grandma.'){
         return("I Love you, too.");
     }
 }
