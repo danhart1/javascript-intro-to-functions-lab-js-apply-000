aa2f2162fca44ff98d28542424d6e8f3c3ac41a6
danhart1 <danhart249@live.com>
2018-08-30 16:02:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if(string.toLowerCase()===string){
-    return("I can't hear you!")else if
+    return("I can't hear you!");else if
     (string.toUpperCase()===string){
       
     }
