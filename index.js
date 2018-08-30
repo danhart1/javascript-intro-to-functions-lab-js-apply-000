c78c3ab71ad0cd7da88bfa4fb8d3fa8e2c027e45
danhart1 <danhart249@live.com>
2018-08-30 15:31:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
-funtction sayHiToGrandma (string){
+funtction ;sayHiToGrandma (string){
   if(string.toLowerCase()===toLowerCase){
     return"I can't hear you!"
   }
